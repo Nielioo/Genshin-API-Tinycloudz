2c75e3a143723d6681cadaf8d6a35b6541a8e7fd
MitchyMichael <84000679+MitchyMichael@users.noreply.github.com>
2022-12-13 21:16:56 +0700

    sampai Today's Domain

--- flutter/genshinapi/lib/views/homepage.dart
+++ flutter/genshinapi/lib/views/homepage.dart
@@ -11,49 +11,126 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.deepPurple, Colors.purpleAccent],
-        )),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
-              child: Column(
-                children: [
-                  Text(
+      body: SingleChildScrollView(
+        child: Container(
+          height: 2000,
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.deepPurple, Colors.purpleAccent],
+            )),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                  child: Text(
                     "Current Banner",
                     style: TextStyle(
                         fontSize: 22,
                         fontWeight: FontWeight.bold,
                         color: Colors.white),
                   ),
-                  Image.asset("assets/images/homeBanner.jpg")
-                ],
-              ),
-              
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                  child: Image.asset(
+                    "assets/images/homeBanner.jpg",
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.fromLTRB(16, 48, 16, 0),
+                  child: Text(
+                    "Today's Domain",
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.fromLTRB(16, 32, 16, 0),
-              child: Text(
-                "Today's Domain",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
