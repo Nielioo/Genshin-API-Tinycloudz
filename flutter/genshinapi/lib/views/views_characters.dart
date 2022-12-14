1d49d65c77fe15cbf68c6b2ee8a713b07926d8e4
MitchyMichael <84000679+MitchyMichael@users.noreply.github.com>
2022-12-14 09:12:39 +0700

    all main pages

--- flutter/genshinapi/lib/views/views_characters.dart
+++ flutter/genshinapi/lib/views/views_characters.dart
@@ -1,7 +1,7 @@
 part of "views.dart";
 
 class ViewsCharacters extends StatefulWidget {
-  const ViewsCharacters({ Key? key }) : super(key: key);
+  const ViewsCharacters({Key? key}) : super(key: key);
 
   @override
   _ViewsCharactersState createState() => _ViewsCharactersState();
@@ -10,8 +10,95 @@ class ViewsCharacters extends StatefulWidget {
 class _ViewsCharactersState extends State<ViewsCharacters> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [MyColor().darkBlue, MyColor().darkPurple],
+        )),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              height: 72,
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              child: Text(
+                "Characters",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 80,
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
