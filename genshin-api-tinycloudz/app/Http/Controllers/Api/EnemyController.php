4e53aad48c39fded9aa8605a230980445718c6c9
annaxatma <83283641+annaxatma@users.noreply.github.com>
2022-12-26 20:42:17 +0700

    Add artifact and enemy api and can see character and enemy details

--- genshin-api-tinycloudz/app/Http/Controllers/Api/EnemyController.php
+++ genshin-api-tinycloudz/app/Http/Controllers/Api/EnemyController.php
@@ -7,16 +7,15 @@ use App\Http\Requests\Api\ApiRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class CharacterController extends Controller
+class EnemyController extends Controller
 {
     public function index(ApiRequest $request)
     {
-        $response = Http::GET('https://api.genshin.dev/characters');
+        $response = Http::GET('https://api.genshin.dev/enemies');
 
         $data = json_decode($response->body());
 
         return $data;
-
     }
 
     public function showById (Request $request)
@@ -26,7 +25,7 @@ class CharacterController extends Controller
 
     public function show(ApiRequest $request, $account_id)
     {
-        $response = Http::GET('https://api.genshin.dev/characters/'.$account_id);
+        $response = Http::GET('https://api.genshin.dev/enemies/'.$account_id);
 
         $data = json_decode($response->body());
 
